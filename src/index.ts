236baa8d563082a5a1d37090848c779b1b612ee7
Richmond Rice <richmond@ricey.org>
2026-08-11 11:58:08 +0100

    address breaking changes in uac service

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,4 @@ export { BusClient } from "./busClient.js";
 
 export { AuthenticationError, BusClientError } from "./errors.js";
 
-export type {
-  Uac,
-  UacChunks,
-  Uacs,
-  UacsByCaseId,
-  UacCount,
-  UacImport,
-  UacEnableDisableResponse,
-} from "./uac.types.js";
+export type { Uac, UacChunks, Uacs, UacsByCaseId, UacCount, UacImport } from "./uac.types.js";
